22ee1e78311c7db9475de66bab4f539263c3178e
ENUM3R <126804321+ENUM3R@users.noreply.github.com>
2024-04-10 21:15:45 +0200

    Update encoder.py
    Dodane kodowanie bitem parzystości

--- encoder.py
+++ encoder.py
@@ -2,8 +2,13 @@ class Encoder:
     def to_packets(self, data: str, packet_size: int):
         return [int(data[i:i+packet_size]) for i in range(0, len(data), packet_size)]
 
-    def to_even_bit(self):
-        return
+    def to_even_bit(data):
+        count = data.count("1")
+        if count % 2 == 0:
+            parity_bit = "0"
+        else:
+            parity_bit = "1"
+        return data + parity_bit
 
     def to_hamming(self):
         return
