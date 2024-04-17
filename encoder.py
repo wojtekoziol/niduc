8b6d6cfcb2fecb8bc2d6b125b2b0fc9ad4bd023f
ENUM3R <126804321+ENUM3R@users.noreply.github.com>
2024-04-17 19:38:06 +0200

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -5,13 +5,13 @@ class Encoder:
     def to_packets(self, data: str, packet_size: int):
         return [int(data[i:i+packet_size]) for i in range(0, len(data), packet_size)]
 
-    def to_even_bit(binary_sequence):
-        count = binary_sequence.count("1")
+    def to_even_bit(data):
+        count = data.count("1")
         if count % 2 == 0:
             parity_bit = "0"
         else:
             parity_bit = "1"
-        return binary_sequence + parity_bit
+        return data + parity_bit
 
     def to_crc8(binary_sequence):
         crc8 = crcmod.predefined.Crc('crc-8')
@@ -20,13 +20,13 @@ class Encoder:
         encoded_sequence = binary_sequence + format(crc_value, '08b')
         return encoded_sequence
 
-    def to_crc16(binary_sequence):
+    def to_crc16(data):
         crc16 = crcmod.predefined.Crc('crc-16')
-        crc16.update(binary_sequence.encode())
+        crc16.update(data.encode())
         crc_value = crc16.crcValue
-        encoded_sequence = binary_sequence + format(crc_value, '016b')
+        encoded_sequence = data + format(crc_value, '016b')
         return encoded_sequence
 
-    def to_crc32(binary_sequence):
-        crc32 = zlib.crc32(binary_sequence.encode())
-        return binary_sequence + format(crc32 & 0xFFFFFFFF, '032b')
+    def to_crc32(data):
+        crc32 = zlib.crc32(data.encode())
+        return data + format(crc32 & 0xFFFFFFFF, '032b')
